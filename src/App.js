04b442d8ebfa39d0a63152e1e651ae6786c83c8b
Donato Rimenti <donatohan.rimenti@gmail.com>
2021-04-19 12:35:39 +0200

    First commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="d-flex flex-column justify-content-center h-100">
+        <Header text="Web tools" />
+    </div>
+  );
+}
+
+export default App;
